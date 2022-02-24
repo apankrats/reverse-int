d5d19949ba8198daa7ee8fcd88eae3a79bbb06ab
apankrats <aliena0798@mail.ru>
2022-02-24 13:01:23 +0300

    init reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+    let len = n.toString().length - 1,
+        result = "";
+    if (n.toString()[len] === "0") {
+        for (i = len - 1 ; i >= 0; i--) {
+            result = result + n.toString()[i];
+        }
+    } else if (n.toString()[0] === "-") {
+        for (i = len; i > 0; i--) {
+            result = result + n.toString()[i];
+        }
+    }else if (n.toString()[len] > 0) {
+        for (i = len; i >= 0; i--) {
+            result = result + n.toString()[i];
+        }
+    }
+    return result
 }
